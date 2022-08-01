c242a5d4f2ed381bbbe629b9ed2a52772fdd4521
Joss Bird <bird.joss@gmail.com>
2022-08-01 16:01:10 +0100

    Add comments to build script

--- build.sh
+++ build.sh
@@ -1,13 +1,18 @@
+#!/usr/bin/env bash
+
+# wetherspoons-pub-fetcher
 cd ./wetherspoons-pub-fetcher
 npm ci
 npm run build
 cd ..
 
+# wetherspoons-menu-fetcher
 cd ./wetherspoons-menu-fetcher
 npm ci
 npm run build
 cd ..
 
+# wetherspoons-api
 cd ./wetherspoons-api
 cd ./venueId-productId
 npm ci
