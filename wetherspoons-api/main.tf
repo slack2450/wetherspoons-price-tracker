7c2b64aed3ddfb21a58276ac4f599bc1a7490f5f
Joss Bird <bird.joss@gmail.com>
2022-08-26 14:20:12 +0100

    Add git commit hooks

--- wetherspoons-api/main.tf
+++ wetherspoons-api/main.tf
@@ -219,8 +219,8 @@ resource "cloudflare_record" "www_spoons_cheap" {
 }
 
 module "venueId_productId" {
-  source        = "./venueId-productId"
+  source         = "./venueId-productId"
   aws_access_key = var.aws_access_key
   aws_secret_key = var.aws_secret_key
-  api_id = aws_apigatewayv2_api.wetherspoons_api.id
+  api_id         = aws_apigatewayv2_api.wetherspoons_api.id
 }
