0731707375da7d1575555fb661c071275d582e51
Joss Bird <bird.joss@gmail.com>
2022-08-26 13:51:52 +0100

    Update menu fetcher lambda function

--- wetherspoons-menu-fetcher/src/app.ts
+++ wetherspoons-menu-fetcher/src/app.ts
@@ -1,91 +1,83 @@
-'use strict';
+"use strict";
 
-import { SQSEvent } from 'aws-lambda';
+import { SQSEvent } from "aws-lambda";
 
-import axios from 'axios';
+import axios from "axios";
 
-axios.defaults.baseURL = 'https://static.wsstack.nn4maws.net';
-
-import { v4 as uuidv4 } from 'uuid';
+axios.defaults.baseURL = "https://static.wsstack.nn4maws.net";
 
 // Create service client module using ES6 syntax.
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
 // Create an Amazon DynamoDB service client object.
-const ddbClient = new DynamoDBClient({ region: 'eu-west-2' });
+const ddbClient = new DynamoDBClient({ region: "eu-west-2" });
 
-import { DynamoDBDocumentClient, BatchWriteCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
 
-const date = (new Date()).setHours(0, 0, 0, 0);
+const date = new Date().setHours(0, 0, 0, 0);
+
+const TableName = "wetherspoons-pubs";
+
+interface Drink {
+  name: string;
+  productId: number;
+  price: number;
+  units: number;
+}
+
+interface Venue {
+  venueId: number;
+  date: number;
+  drinks: Drink[];
+}
 
 export const handler = async (event: SQSEvent): Promise<void> => {
+  for (const record of event.Records) {
+    const notification = JSON.parse(record.body);
+    const inputData = JSON.parse(notification.Message);
 
-    for (const record of event.Records) {
+    const {
+      data: { menus },
+    } = await axios.get(`/content/v3/menus/${inputData.venueId}.json`);
 
-        const notification = JSON.parse(record.body);
+    const productsInserted: number[] = [];
 
-        const venue = JSON.parse(notification.Message);
+    const venue: Venue = {
+      venueId: inputData.venueId,
+      date,
+      drinks: [],
+    };
 
-        const { data: { menus } } = await axios.get(`/content/v3/menus/${venue.venueId}.json`);
+    for (const menu of menus) {
+      if (menu.name != "Drinks") continue;
 
-        const productsInserted: string[] = [];
-        const putRequests: any[] = [];
+      for (const subMenu of menu.subMenu) {
+        for (const productGroup of subMenu.productGroups) {
+          for (const product of productGroup.products) {
+            if (productsInserted.indexOf(product.productId) > -1) continue;
 
-        for (const menu of menus) {
-            if (menu.name != 'Drinks')
-                continue;
+            const regex = /ABV, (...) unit/;
+            const matches = product.description.matches(regex);
+            const units = parseFloat(matches[1]);
 
-            for (const subMenu of menu.subMenu) {
-                for (const productGroup of subMenu.productGroups) {
-                    for (const product of productGroup.products) {
-                        if (productsInserted.indexOf(product.productId) > -1)
-                            continue;
+            productsInserted.push(product.productId);
 
-                        productsInserted.push(product.productId);
-
-                        putRequests.push({
-                            PutRequest: {
-                                Item: {
-                                    id: uuidv4(),
-                                    venueIdProductId: `${venue.venueId}-${product.productId}`,
-                                    timestamp: date,
-                                    name: product.eposName,
-                                    venueId: venue.venueId,
-                                    productId: product.productId,
-                                    price: product.priceValue,
-                                }
-                            }
-                        });
-                    }
-                }
-            }
+            venue.drinks.push({
+              name: product.eposName,
+              productId: product.productId,
+              price: product.priceValue,
+              units,
+            });
+          }
         }
-
-        // Split PutRequests into batches of 25 as that's the DynamoDB max size
-        const maxDynamoDBBatchSize = 25
-        const commands: BatchWriteCommand[] = [];
-        for (let i = 0; i < putRequests.length; i += maxDynamoDBBatchSize) {
-            const chunk = putRequests.slice(i, i + maxDynamoDBBatchSize);
-
-            commands.push(new BatchWriteCommand({
-                RequestItems: {
-                    'wetherspoons-drinks': chunk
-                }
-            }));
-        }
-
-        console.log(`Split ${putRequests.length} put requests into ${commands.length} commands`);
-
-        const commandExecutions: Promise<any>[] = [];
-        for(const command of commands as any[]) {
-            console.log(`Sending batch of ${command.input.RequestItems['wetherspoons-drinks'].length}`);
-
-            commandExecutions.push(ddbDocClient.send(command));
-        }
-
-        await Promise.all(commandExecutions);
+      }
     }
 
-    return;
-}
+    const command = new PutCommand({ TableName, Item: venue });
+
+    await ddbDocClient.send(command);
+  }
+
+  return;
+};
