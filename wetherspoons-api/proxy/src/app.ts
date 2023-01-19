60b59fa4bfce13927262b41e3176c8e44417c518
Joss Bird <bird.joss@gmail.com>
2023-01-19 18:13:53 +0000

    Update proxy implementation

--- wetherspoons-api/proxy/src/app.ts
+++ wetherspoons-api/proxy/src/app.ts
@@ -8,7 +8,7 @@ export const handler = async (event: APIGatewayEvent ) => {
     if(!event.pathParameters)
         return;
 
-    const data = axios.get(`/${event.pathParameters.proxy}`);
+    const response = await axios.get(`/${event.pathParameters.proxy}`);
     
-    return data;
+    return response.data;
 }
