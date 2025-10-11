d76f7829218230498cbb4948397e3cefa1ad9d2d
Joss Bird <bird.joss@gmail.com>
2025-10-11 19:53:05 +0100

    Increase timeout of menu fetcher

--- wetherspoons-menu-fetcher/main.tf
+++ wetherspoons-menu-fetcher/main.tf
@@ -103,7 +103,7 @@ resource "aws_lambda_function" "wetherspoons_menu_fetcher" {
   reserved_concurrent_executions = -1
   role                           = aws_iam_role.wetherspoons_menu_fetcher_role.arn
   runtime                        = "nodejs18.x"
-  timeout                        = 30
+  timeout                        = 120
 
   environment {
     variables = {
